f44c6d7ada15a9fae7b20e205456426093c14c54
Arman <golopichu@gmail.com>
2025-11-19 22:30:30 +0400

    Create templates

--- onlinelib/home/views.py
+++ onlinelib/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+menu = [
+    { 'title': 'Главная страница', 'url_name': 'home' },
+    { 'title': 'О себе', 'url_name': 'about' },
+]
+
+def index(request):
+    data = {
+        'menu': menu,
+    }
+    return render(request, 'home/index.html', context=data)
+
+def about(request):
+    data = {
+        'menu': menu,
+    }
+    return render(request, 'home/about.html', context=data)
