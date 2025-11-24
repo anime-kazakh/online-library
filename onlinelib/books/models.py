e7d90ac50bd48bd23c5f62858106fa04ca3c35e1
Arman <golopichu@gmail.com>
2025-11-24 17:59:24 +0400

    Model added for book app

--- onlinelib/books/models.py
+++ onlinelib/books/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    original_title = models.CharField(max_length=255)
+    cover_image = models.ImageField(
+        upload_to='images/%Y/%m/%d/',
+        default=None,
+        blank=True,
+        null=True
+    )
+    description = models.TextField()
+    publication_year = models.IntegerField()
+    isbn = models.TextField()
+    upload_date = models.DateField(auto_now_add=True, blank=False, null=False)
+    views_count = models.IntegerField(default=0)
+    status_type = models.TextChoices('status_type', 'available unavailable')
+    status = models.TextField(choices=status_type, default='available')
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    code = models.CharField(max_length=3, null=False, blank=False, unique=True)
+
+
+class Files(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book_file = models.FileField(
+        upload_to='files/%Y/%m/%d/',
+        default=None,
+        blank=False,
+        null=False,
+    )
+    file_size = models.IntegerField(default=0)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    upload_date = models.DateField(auto_now_add=True, blank=False, null=False)
+    download_count = models.IntegerField(default=0)
+
