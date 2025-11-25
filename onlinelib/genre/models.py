801a1f1b317e952f919b99d2752c3594b36878e8
Arman <golopichu@gmail.com>
2025-11-25 17:26:22 +0400

    Added genre model

--- onlinelib/genre/models.py
+++ onlinelib/genre/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        unique=True,
+    )
+    description = models.TextField()
