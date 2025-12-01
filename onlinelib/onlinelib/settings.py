3784277374ca9a97eebe69f1c826a22ecf2370bb
Arman <golopichu@gmail.com>
2025-12-01 04:42:41 +0400

    set dotenv on debug

--- onlinelib/onlinelib/settings.py
+++ onlinelib/onlinelib/settings.py
@@ -17,14 +17,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=os.getenv("SECRET_KEY")
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+if DEBUG:
+    import dotenv
+    dotenv.load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY=os.getenv("SECRET_KEY")
 
 ALLOWED_HOSTS = [
     'localhost',
