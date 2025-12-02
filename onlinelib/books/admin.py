087fcabe1f66575f4dfa536f84c8cf2f295d88cc
Arman <golopichu@gmail.com>
2025-12-02 19:12:38 +0400

    Deleted reg_list

--- onlinelib/books/admin.py
+++ onlinelib/books/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Book, Language, Files
 
 # Register your models here.
-admin.site.register([Book, Language, Files])
-
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'original_title',
