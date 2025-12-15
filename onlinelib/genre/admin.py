f6abfc686b54ef947ac09621fa4e8ba5f691bb68
Arman <golopichu@gmail.com>
2025-12-15 07:57:41 +0400

    Added Tag, Content Warnings, Age Rating models. Refactored Genres model, added complicated hierarchy of genres, added nested level

--- onlinelib/genre/admin.py
+++ onlinelib/genre/admin.py
@@ -1,12 +1,55 @@
-from django.contrib import admin
-from .models import Genre
+from django.contrib import admin, messages
+
+from .models import Genre, Tag, ContentWarning, AgeRating
 
 # Register your models here.
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug')
-    list_display_links = ('id', 'name')
+    list_display = ('name', 'slug', 'level', 'status')
+    list_display_links = ('name', )
     # readonly_fields = ('slug',)
     prepopulated_fields = {'slug': ('name',)}
     list_per_page = 20
     search_fields = ('name',)
+    list_editable = ('level', 'status')
+    list_filter = ('level', )
+    actions = ('set_status_available', 'set_status_unavailable')
+
+    @admin.action(description='Опубликовать')
+    def set_status_available(self, request, queryset):
+        count = queryset.update(status=Genre.StatusType.AVAILABLE)
+        self.message_user(request, f'Изменено {count} записей!')
+
+    @admin.action(description='Снять с публикации')
+    def set_status_unavailable(self, request, queryset):
+        count = queryset.update(status=Genre.StatusType.UNAVAILABLE)
+        self.message_user(request, f'Изменено {count} записей!',
+                          messages.WARNING)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name', )
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+@admin.register(ContentWarning)
+class ContentWarningAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name', )
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+@admin.register(AgeRating)
+class AgeRatingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'min_age', 'max_age')
+    list_display_links = ('name', )
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'min_age', 'max_age')
+    list_editable = ('min_age', 'max_age')
+    list_per_page = 20
--- onlinelib/genre/models.py
+++ onlinelib/genre/models.py
@@ -1,21 +1,42 @@
 from django.db import models
 from django.urls import reverse
 
-# from transliterate import slugify
-
 # Create your models here.
 class Genre(models.Model):
     name = models.CharField( max_length=255, blank=False, null=False,
                              unique=True, verbose_name="Жанр")
     slug = models.SlugField(max_length=255, unique=True, db_index=True,
                             verbose_name="slug")
-    description = models.TextField(blank=True, null=True,
-                                   verbose_name="Описание")
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+
+    class LevelType(models.TextChoices):
+        ROOT = 'root', 'Корневой жанр'
+        GROUP = 'group', 'Группа'
+        GENERAL = 'general', 'Общий жанр'
+        MAIN = 'main', 'Жанр'
+        SUB = 'sub', 'Поджанр'
+
+
+    level = models.CharField(max_length=255, choices=LevelType,
+                             null=False, blank=False,
+                             verbose_name="Уровень вложенности")
+
+    class StatusType(models.TextChoices):
+        AVAILABLE = 'available', 'Активная'
+        UNAVAILABLE = 'unavailable', 'Неактивная'
+
+
+    status = models.CharField(max_length=255, choices=StatusType,
+                              default=StatusType.AVAILABLE, verbose_name="Статус")
+    parent = models.ForeignKey('self', null=True, blank=True,
+                               related_name='children', related_query_name='child',
+                               verbose_name="Родитель", on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = "Жанр"
         verbose_name_plural = "Жанры"
-        ordering = ['name']
+        ordering = ['level']
+
 
     def __str__(self):
         return self.name
@@ -24,5 +45,59 @@ class Genre(models.Model):
         return reverse('genre-page', kwargs={ 'genre_slug': self.slug })
 
     # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.name)
+    #     level = 0
+    #     parent = self.parent
+    #     while parent is not None:
+    #         level += 1
+    #         parent = parent.parent
+    #     self.level = level
     #     super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, blank=False,
+                            null=False, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True,
+                            db_index=True, verbose_name="slug")
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ['name']
+
+
+    def __str__(self):
+        return self.name
+
+
+class ContentWarning(models.Model):
+    name = models.CharField(max_length=255, blank=False,
+                            null=False, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True,
+                            null=False, blank=False,
+                            db_index=True, verbose_name="slug")
+
+    class Meta:
+        verbose_name = "Предупреждение о содержании"
+        verbose_name_plural = "Предупреждения о содержании"
+        ordering = ['name']
+
+
+    def __str__(self):
+        return self.name
+
+
+class AgeRating(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="slug")
+    min_age = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name="Минимальный возраст")
+    max_age = models.PositiveSmallIntegerField(null=False, blank=False, verbose_name="Максимальный возраст")
+
+    class Meta:
+        verbose_name = "Возрастной рейтинг"
+        verbose_name_plural = "Возрастные рейтинги"
+        ordering = ['min_age']
+
+
+    def __str__(self):
+        return self.name
