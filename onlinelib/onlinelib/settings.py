bf27f0b5118af8596398af00700ce25a8259835d
Arman <golopichu@gmail.com>
2026-02-12 22:22:43 +0400

    Added password reset with email

--- onlinelib/onlinelib/settings.py
+++ onlinelib/onlinelib/settings.py
@@ -176,3 +176,17 @@ CACHES = {
         }
     }
 }
+
+# SMTP
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'tdjangoproject@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
