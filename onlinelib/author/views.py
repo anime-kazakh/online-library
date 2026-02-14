6ee221e381348aa95c3f60c70da30f9bf5e7acf8
Arman <golopichu@gmail.com>
2026-02-14 15:48:42 +0400

    Added permission at author

--- onlinelib/author/views.py
+++ onlinelib/author/views.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.core.exceptions import PermissionDenied
+
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -22,26 +24,42 @@ class AuthorPage(DataMixin, DetailView):
     slug_url_kwarg = 'author_slug'
 
 
-class AddAuthor(LoginRequiredMixin, DataMixin, CreateView):
+class AddAuthor(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView):
     form_class = AuthorForm
     template_name = 'author/add_author.html'
     title_page = 'Добавление автора'
     # login_url = 'users:login'
+    permission_required = 'author.add_author'  # <app>.<action>_<tab>
 
     def form_valid(self, form):
         w = form.save(commit=False)
         w.post_author = self.request.user
         return super().form_valid(form)
 
-class UpdateAuthor(LoginRequiredMixin, DataMixin, UpdateView):
+
+class UpdateAuthor(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, UpdateView):
     model = Author
     form_class = AuthorForm
     template_name = 'author/add_author.html'
     title_page = 'Редактирование автора'
+    permission_required = 'author.change_author'
+
+    def get_object(self, queryset=None):
+        author = super().get_object(queryset)
+        if self.request.user != author.post_author:
+            raise PermissionDenied
+        return author
 
 
-class DeleteAuthor(LoginRequiredMixin, DataMixin, DeleteView):
+class DeleteAuthor(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, DeleteView):
     model = Author
     template_name = 'author/delete_confirm.html'
     success_url = reverse_lazy('author-home')
     title_page = 'Удаление автора'
+    permission_required = 'author.delete_author'
+
+    def get_object(self, queryset=None):
+        author = super().get_object(queryset)
+        if self.request.user != author.post_author:
+            raise PermissionDenied
+        return author
