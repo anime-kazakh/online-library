288aedbc70d40f6cbcc91f24f95c9b54bd05648e
Arman <golopichu@gmail.com>
2026-02-26 16:26:29 +0400

    Preparing on PostgreSQL

--- onlinelib/onlinelib/settings.py
+++ onlinelib/onlinelib/settings.py
@@ -97,11 +97,22 @@ WSGI_APPLICATION = 'onlinelib.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', 5432),
+    },
 }
 
 # Password validation
