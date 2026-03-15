597dddd692c5e7c33ee9371ebbf1928dfe7bf0ea
Arman <golopichu@gmail.com>
2026-03-15 13:13:30 +0400

    add: nested serializers

--- onlinelib/books/serializers.py
+++ onlinelib/books/serializers.py
@@ -3,15 +3,6 @@ from rest_framework import serializers
 from .models import Book, Files, Language
 
 
-class BookSerializer(serializers.ModelSerializer):
-    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
-    class Meta:
-        model = Book
-        fields = '__all__'
-        read_only_fields = ('upload_date', 'user')
-
-
 class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
@@ -20,8 +11,26 @@ class LanguageSerializer(serializers.ModelSerializer):
 
 class FileSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    language = LanguageSerializer(read_only=True)
 
     class Meta:
         model = Files
+        fields = '__all__'
+        read_only_fields = ('upload_date', 'user')
+
+
+class AuthorSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    full_name = serializers.CharField()
+    slug = serializers.CharField()
+
+
+class BookSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    files = FileSerializer(many=True, read_only=True)
+    authors = AuthorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Book
         fields = '__all__'
         read_only_fields = ('upload_date', 'user')
