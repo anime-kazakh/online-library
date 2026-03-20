236ab40d6c3a2410fceeaab7aff40917f1c1e2f8
Arman <golopichu@gmail.com>
2026-03-21 00:13:28 +0400

    add: superuser fixture

--- onlinelib/author/tests/conftest.py
+++ onlinelib/author/tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
+from django.contrib.auth.models import Group, Permission
 
+from users.tests.factories import UserFactory
 from .factories import AuthorFactory
 
 
 @pytest.fixture
 def author():
     return AuthorFactory()
+
+@pytest.fixture
+def superuser():
+    return UserFactory(admin=True)
